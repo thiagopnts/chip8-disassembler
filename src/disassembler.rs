00b8ca94f430f806c3f4f0da82ac348dfa8c8b4a
thiagopnts <thiagopnts@gmail.com>
2014-07-15 18:36:07 -0300

    union 8 bit data into an 16bit config var

--- src/disassembler.rs
+++ src/disassembler.rs
@@ -7,40 +7,40 @@ use std::os;
 fn disassemble(buffer: Vec<u8>) {
   let mut address: int = 0x200;
   for i in range_step(0, buffer.len(), 2) {
-    let first_part = *buffer.get(i);
-    let second_part = *buffer.get(i + 1);
-    let instruction = match first_part >> 4 {
-      0x0 => match second_part {
+    let config = *buffer.get(i) as u16 | *buffer.get(i + 1) as u16 << 8;
+    println!("{} {}", config & 0x00FF, config >> 8);
+    let instruction = match (config & 0x00FF) >> 4 {
+      0x0 => match config >> 8 {
         0xE0 => "CLS".to_string(),
         0xEE => "RTS".to_string(),
         _    => "unkwown".to_string(),
       },
-      0x1   => format!("JUMP ${:01x}{:02x}", first_part & 0xF, second_part),
-      0x2   => format!("CALL ${:01x}{:02x}", first_part & 0xF, second_part),
-      0x3   => format!("SKIP.EQ V{:01X},#${:02x}", first_part & 0xF, second_part),
-      0x4   => format!("SKIP.NE V{:01X},#${:02x}", first_part & 0xF, second_part),
-      0x5   => format!("SKIP.EQ V{:01X},V{:01X}", first_part & 0xF, second_part >> 4),
-      0x6   => format!("MVI V{:01X},#${:02x}", first_part & 0xF, second_part),
-      0x7   => format!("ADI V{:01X},#${:02x}", first_part & 0xF, second_part),
-      0x8   => match first_part >> 4 {
-        0 => format!("MOV. V{:01X},V{:01X}", first_part & 0x0F, second_part >> 4),
-        1 => format!("OR. V{:01X},V{:01X}", first_part & 0x0F, second_part >> 4),
-        2 => format!("AND. V{:01X},V{:01X}", first_part & 0x0F, second_part >> 4),
-        3 => format!("XOR. V{:01X},V{:01X}", first_part & 0x0F, second_part >> 4),
-        4 => format!("ADD. V{:01X},V{:01X}", first_part & 0x0F, second_part >> 4),
-        5 => format!("SUB. V{:01X},V{:01X},V{:01X}", first_part & 0x0F, first_part & 0x0F, second_part >> 4),
-        6 => format!("SHR. V{:01X},V{:01X}", first_part & 0x0F, second_part >> 4),
-        7 => format!("SUB. V{:01X},V{:01X},V{:01X}", first_part & 0x0F, second_part >> 4, second_part >> 4),
-        0xE => format!("SHL. V{:01X},V{:01X}", first_part & 0x0F, second_part >> 4),
+      0x1   => format!("JUMP ${:01x}{:02x}", (config & 0x00FF) & 0xF, config >> 8),
+      0x2   => format!("CALL ${:01x}{:02x}", (config & 0x00FF) & 0xF, config >> 8),
+      0x3   => format!("SKIP.EQ V{:01X},#${:02x}", (config & 0x00FF) & 0xF, config >> 8),
+      0x4   => format!("SKIP.NE V{:01X},#${:02x}", (config & 0x00FF) & 0xF, config >> 8),
+      0x5   => format!("SKIP.EQ V{:01X},V{:01X}", (config & 0x00FF) & 0xF, (config >> 8) >> 4),
+      0x6   => format!("MVI V{:01X},#${:02x}", (config & 0x00FF) & 0xF, config >> 8),
+      0x7   => format!("ADI V{:01X},#${:02x}", (config & 0x00FF) & 0xF, config >> 8),
+      0x8   => match (config & 0x00FF) >> 4 {
+        0 => format!("MOV. V{:01X},V{:01X}", (config & 0x00FF) & 0x0F, (config >> 8) >> 4),
+        1 => format!("OR. V{:01X},V{:01X}", (config & 0x00FF) & 0x0F, (config >> 8) >> 4),
+        2 => format!("AND. V{:01X},V{:01X}", (config & 0x00FF) & 0x0F, (config >> 8) >> 4),
+        3 => format!("XOR. V{:01X},V{:01X}", (config & 0x00FF) & 0x0F, (config >> 8) >> 4),
+        4 => format!("ADD. V{:01X},V{:01X}", (config & 0x00FF) & 0x0F, (config >> 8) >> 4),
+        5 => format!("SUB. V{:01X},V{:01X},V{:01X}", (config & 0x00FF) & 0x0F, (config & 0x00FF) & 0x0F, (config >> 8) >> 4),
+        6 => format!("SHR. V{:01X},V{:01X}", (config & 0x00FF) & 0x0F, (config >> 8) >> 4),
+        7 => format!("SUB. V{:01X},V{:01X},V{:01X}", (config & 0x00FF) & 0x0F, (config >> 8) >> 4, (config >> 8) >> 4),
+        0xE => format!("SHL. V{:01X},V{:01X}", (config & 0x00FF) & 0x0F, (config >> 8) >> 4),
         _   => "unknown".to_string(),
       },
-      0x9 => format!("SKIP.NE V{:01X},V{:01X}", first_part & 0x0F, second_part >> 4),
-      0xA => format!("MVI I,#${:01x}{:02x}", first_part & 0x0F, second_part),
-      0xB => format!("JUMP ${:01x}{:02x}(V0)", first_part & 0x0F, second_part),
-      0xC => format!("RNDMSK V{:01X},#${:02x}", first_part & 0x0F, second_part),
+      0x9 => format!("SKIP.NE V{:01X},V{:01X}", (config & 0x00FF) & 0x0F, (config >> 8) >> 4),
+      0xA => format!("MVI I,#${:01x}{:02x}", (config & 0x00FF) & 0x0F, config >> 8),
+      0xB => format!("JUMP ${:01x}{:02x}(V0)", (config & 0x00FF) & 0x0F, config >> 8),
+      0xC => format!("RNDMSK V{:01X},#${:02x}", (config & 0x00FF) & 0x0F, config >> 8),
       _     => "not implemented".to_string(),
     };
-    println!("{:04x} {:02x} {:02x} {}", address, first_part, second_part, instruction);
+    println!("{:04x} {:02x} {:02x} {}", address, (config & 0x00FF), config >> 8, instruction);
     address += 2;
   }
 }
